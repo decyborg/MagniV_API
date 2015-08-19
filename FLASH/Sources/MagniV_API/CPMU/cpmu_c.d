dcf59c2420f6d15f9c0798386d7094adb7c3232e
Manuel Rodriguez <manuel2982@gmail.com>
2015-08-19 10:35:57 -0500

    Cleaned repository

--- FLASH/Sources/MagniV_API/CPMU/cpmu_c.d
+++
@@ -1,6 +0,0 @@
-Sources/MagniV_API/CPMU/cpmu_c.obj: C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\CPMU\cpmu.c \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\CPMU\cpmu.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Project_Headers\derivative.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Project_Headers\mc9s12zvl32.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\common.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\hw_def.h 
--- FLASH/Sources/MagniV_API/SCI/sci_c.d
+++
@@ -1,9 +0,0 @@
-Sources/MagniV_API/SCI/sci_c.obj: C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\SCI\sci.c \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\SCI\sci.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Project_Headers\derivative.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Project_Headers\mc9s12zvl32.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\common.h \
-	C:\Users\b52932\Documents\MagniV_API\MagniV_API\Sources\MagniV_API\hw_def.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\S12lisa_Support\s12lisac\include\hidef.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\S12lisa_Support\s12lisac\include\stdtypes.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\S12lisa_Support\s12lisac\include\stddef.h 
