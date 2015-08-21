977aa97a830c3d16715f7b122a65b57db8a3c409
Manuel Rodriguez <manuel2982@gmail.com>
2015-08-21 11:03:55 -0500

    mend

---
+++ Documentation/html/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tranfer_5fspi',['tranfer_SPI',['../group___s_p_i.html#ga0161f7f2a3f5e77e2e71051bdfdcb856',1,'spi.c']]]
+];
---
+++ Documentation/html/search/variables_12.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['xfrw',['xfrw',['../unionu_s_p_i_c_r2.html#a88ee7588bdfe8dc1f0e11bec64885042',1,'uSPICR2']]]
+];
